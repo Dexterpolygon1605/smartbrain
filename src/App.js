e0a92ffe73ca85e2ed754f8ae5b682de90bfa9c3
Dexterpolygon1605 <nelsonlopescp2020@gmail.com>
2021-11-02 18:33:13 +0000

    face detection box

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './Components/Navigation/Navigation';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
-import Clarifai, { FACE_DETECT_MODEL } from 'clarifai';
 import './App.css';
 
 const app = new Clarifai.App({
-  apiKey: 'c65f7faee63a448094f2877f83286f17'
+  apiKey: 'eec993a105a9404b99f636c6d722dda0'
 });
 
 const particlesOptions = {
@@ -30,25 +30,41 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
 
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () =>{
-  this.setState({imageUrl: this.state.input})
-  app.models.predict(Clarifai.FACE_DETECT_MODEL,
-   this.state.input).then(
-    function(response){
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    },
-    function(err){
-
-    }
-  )
+  this.setState({imageUrl: this.state.input});
+  app.models
+  .predict(
+    "a403429f2ddf4b49b307e318f00e528b",
+   this.state.input)
+   .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+   .catch(err => console.log(err))
 }
 
   render(){
@@ -62,7 +78,7 @@ class App extends Component {
       <Logo />
       <Rank />
       <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-     <FaceRecognition imageUrl={this.state.imageUrl}/>
+     <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
     </div>
   )};
 }
